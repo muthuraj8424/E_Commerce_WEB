7622ce510ba471801bbc24a6182c7a50ead9b895
MUTHURAJ K <139791385+muthuraj8424@users.noreply.github.com>
2024-12-24 23:16:22 +0530

    Update DeatiledInfo.jsx

--- Client/src/pages/DeatiledInfo.jsx
+++ Client/src/pages/DeatiledInfo.jsx
@@ -115,7 +115,7 @@ function DetailedInfo() {
         {/* Product Image */}
         <div className="sm:w-1/2">
           <img
-            src={`http://localhost:5000${product.photos[0]}`}
+            src={`https://e-com-backend-65l1.onrender.com${product.photos[0]}`}
             alt={product.name}
             className="w-full h-96 object-cover rounded-lg shadow-lg transform transition-all duration-300 hover:scale-105"
           />
