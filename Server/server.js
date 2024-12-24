1b908ef06d6b4598846bd528cd348d7974e7fca4
MUTHURAJ K <139791385+muthuraj8424@users.noreply.github.com>
2024-12-24 23:20:00 +0530

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -54,12 +54,14 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 const corsOptions = {
-  origin: ['http://localhost:5173'], 
+  origin: ['https://e-com-frontend-b66t.onrender.com'], 
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
   credentials: true, 
   preflightContinue: false, 
   allowedHeaders: ['Content-Type', 'Authorization'] 
 };
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions)); 
 app.use(cookieParser())
 app.use(cors(corsOptions));
 app.use("/uploads", express.static(path.join(__dirname, "routes", "uploads")));
